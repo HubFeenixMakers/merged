d1ecda07c408d60cd7d5c405b7e3a44537094ae3
Torsten <torsten@rubydesign.fi>
2022-11-29 20:03:48 +0200

    section template selection

--- app/controllers/merged/sections_controller.rb
+++ app/controllers/merged/sections_controller.rb
@@ -1,13 +1,23 @@
 module Merged
   class SectionsController < MergedController
-    before_action :set_page, only: %i[ show edit update destroy set_image select_image]
+    before_action :set_page
+    #, only: %i[ show edit update destroy set_image select_image]
 
     def select_image
       @images = Image.all
     end
+    def select_template
+      @sections = Style.sections
+    end
 
     def set_image
       @section.content["image"] = params[:image]
+      @page.save
+      redirect_to page_section_url(@page.id,@section.id)
+    end
+
+    def set_template
+      @section.content["template"] = params[:template]
       @page.save
       redirect_to page_section_url(@page.id,@section.id)
     end
@@ -21,7 +31,7 @@ module Merged
         end
       end
       @page.save
-      redirect_to :merged_page_section
+      redirect_to :page_section
     end
 
     private
